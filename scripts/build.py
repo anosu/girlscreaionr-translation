766b2956496d099ebe68cac310dbbdf4cc75dba6
Jitsu <jitsu233@gmail.com>
2026-06-06 21:08:23 +0800

    adapt multi languages

--- scripts/build.py
+++ scripts/build.py
@@ -3,6 +3,7 @@ import json
 from pathlib import Path
 
 trans_dir = Path(".")
+language = "zh_Hans"
 
 
 def get_hash(obj) -> str:
@@ -17,17 +18,17 @@ def hash_file(path: Path) -> str:
 
 
 manifest = {
-    "names": hash_file(trans_dir / "names/zh_Hans.json"),
-    "words": hash_file(trans_dir / "words/zh_Hans.json"),
+    "names": hash_file(trans_dir / f"names/{language}.json"),
+    "words": hash_file(trans_dir / f"words/{language}.json"),
     "novels": {
         int(f.parent.name): hash_file(f)
-        for f in trans_dir.glob("novels/*/zh_Hans.json")
+        for f in trans_dir.glob(f"novels/*/{language}.json")
     },
 }
 
 manifest["hash"] = get_hash(manifest)
 
-manifest_path = trans_dir / "manifest.json"
+manifest_path = trans_dir / f"manifest/{language}.json"
 manifest_path.write_text(
     json.dumps(manifest, sort_keys=True, ensure_ascii=False, indent=4), encoding="utf-8"
 )
